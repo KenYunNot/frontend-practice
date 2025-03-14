d72214729c6b7440bbf06e6cb161a40d4f8701c6
Ken Yun <yunhaen@gmail.com>
2025-03-14 14:14:09 -0400

    feat: created routes for home and card-hover components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './App.css'
+import CardHover from './components/CardHover'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/card-hover' element={<CardHover />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
